0913f4891c425283128a565ecd242b51e0a05808
Alexandre <136007010+ZokszY@users.noreply.github.com>
2024-06-19 10:24:24 +0200

    Small fix

--- src/training.py
+++ src/training.py
@@ -207,18 +207,10 @@ def get_perfect_preds(
         20 * nn.functional.one_hot(torch.tensor(cls), num_classes=num_classes).to(device) - 0.5
         for cls in extracted_classes
     ]
-    print(f"{len(extracted_bboxes[0]) = }")
-    print(f"{torch.stack(extracted_bboxes[0], dim=0).shape = }")
-    print(f"{extracted_bboxes[0][0].shape = }")
-    print(f"{extracted_scores[0].shape = }")
-    print(
-        f"{torch.cat((torch.stack(extracted_bboxes[0], dim=0), extracted_scores[0]), dim=1).shape = }"
-    )
     perfect_preds = [
-        torch.cat((torch.stack(bboxes, dim=0), scores), dim=1).permute((1, 0)).unsqueeze(0)
+        torch.cat((torch.stack(bboxes, dim=0), scores), dim=1).unsqueeze(0)
         for bboxes, scores in zip(extracted_bboxes, extracted_scores)
     ]
-    print(f"{perfect_preds[0].shape = }")
     perfect_preds = torch.cat(
         [
             torch.cat(
