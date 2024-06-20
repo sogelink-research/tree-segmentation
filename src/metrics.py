Fixes AP metrics computation

src/metrics.py
@@ -1,11 +1,9 @@
-import itertools
 from typing import List, Optional, Tuple, TypeVar, cast
 
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from scipy.optimize import linear_sum_assignment
-from skimage import io
 
 from box_cls import Box, compute_iou
 from dataloaders import convert_ground_truth_from_tensors
@@ -43,10 +41,11 @@ def hungarian_algorithm(
 
     # Extract pairs of indices where matches were found
     matched_pairs = [
-        ((pred, gt), float(cost_matrix[pred, gt]))
+        ((pred, gt), float(1 - cost_matrix[pred, gt]))
         for pred, gt in zip(pred_ind, gt_ind)
         if pred < pred_len and gt < gt_len and cost_matrix[pred, gt] != max_cost
     ]
+    matched_pairs.sort(key=lambda t: t[1])
 
     unmatched_pred = list(
         set(range(pred_len)).difference(set(map(lambda t: t[0][0], matched_pairs)))
@@ -89,7 +88,8 @@ def compute_sorted_ap(
     unmatched_pred: List[int],
     unmatched_gt: List[int],
 ) -> Tuple[List[float], List[float], float]:
-    sorted_ious = sorted(map(lambda t: t[1], matched_pairs))
+    matched_pairs.sort(key=lambda t: t[1])
+    sorted_ious = list(map(lambda t: t[1], matched_pairs))
 
     tp0 = len(matched_pairs)
     fp0 = len(unmatched_pred)
@@ -251,7 +251,7 @@ class AP_Metrics:
             image_indices=image_indices,
         )
 
-        # Compute the matchings
+        # Compute the matchings for each individual image
         iou_threshold = 1e-6
         for bboxes, scores, classes_as_ints, gt_bboxes_list, gt_classes_list in zip(
             bboxes_list,
@@ -277,7 +277,7 @@ class AP_Metrics:
 
         self.sorted_ap_updated = False
 
-    def _compute_sorted_ap(self) -> None:
+    def compute_sorted_ap(self) -> None:
         if not self.sorted_ap_updated:
             self.sorted_ious_list, self.aps_list, self.sorted_ap_list = compute_sorted_ap_confs(
                 self.matched_pairs, self.unmatched_pred, self.unmatched_gt
@@ -302,7 +302,7 @@ class AP_Metrics:
             - conf_threshold_list contains the model confidence thresholds used to compute the
             metrics above.
         """
-        self._compute_sorted_ap()
+        self.compute_sorted_ap()
         return self.sorted_ious_list, self.aps_list, self.sorted_ap_list, self.conf_threshold_list
 
     def get_best_sorted_ap(self) -> Tuple[List[float], List[float], float, float]:
@@ -318,7 +318,7 @@ class AP_Metrics:
             - sorted_ap is the sortedAP metrics (the integral of aps w.r.t. sorted_ious).
             - conf_threshold is the corresponding model confidence threshold.
         """
-        self._compute_sorted_ap()
+        self.compute_sorted_ap()
         best_index = self.sorted_ap_list.index(max(self.sorted_ap_list))
         best_sorted_ious = self.sorted_ious_list[best_index]
         best_aps = self.aps_list[best_index]
@@ -347,6 +347,7 @@ class AP_Metrics_List:
         self.legend_list = [] if legend_list is None else legend_list
 
     def add_ap_metrics(self, ap_metrics: AP_Metrics, legend) -> None:
+        ap_metrics.compute_sorted_ap()
         self.ap_metrics_list.append(ap_metrics)
         self.legend_list.append(legend)
 
@@ -380,6 +381,7 @@ class AP_Metrics_List:
         conf_threshold_lists = []
 
         for ap_metrics in self.ap_metrics_list:
+            ap_metrics.compute_sorted_ap()
             sorted_ap_lists.append(ap_metrics.sorted_ap_list)
             conf_threshold_lists.append(ap_metrics.conf_threshold_list)
 

src/run.py
@@ -529,6 +529,8 @@ def simple_test():
     image_chm = image_chm_initial.astype(np.float32)
     image_chm = torch.from_numpy(image_chm).permute((2, 0, 1)).unsqueeze(0).to(device)
 
+    ap_metrics_list = AP_Metrics_List()
+
     epochs = 100
     for epoch in range(1, epochs + 1):
         thresholds_low = np.power(10, np.linspace(-4, -1, 10))
@@ -545,7 +547,6 @@ def simple_test():
 
         with torch.no_grad():
             preds = model.preds_from_output(output)
-            old_preds = preds.detach().clone()
             ap_metrics.add_preds(
                 model=model,
                 preds=preds,
@@ -555,7 +556,7 @@ def simple_test():
                 image_indices=image_indices,
             )
 
-            if epoch % 1 == 0:
+            if epoch % 10 == 0:
                 dataset_idx = 0
                 if dataset_idx in image_indices.tolist():
                     batch_idx = image_indices.tolist().index(dataset_idx)
@@ -588,11 +589,13 @@ def simple_test():
                         colors_dict=DatasetConst.CLASS_COLORS.value,
                         save_path=f"Data_epoch_{epoch}_train.png",
                     )
-        print(f"{(preds - old_preds).abs().sum() = }")
 
         optimizer.step()
         optimizer.zero_grad()
         scheduler.step()
+
+        if epoch % 10 == 0:
+            ap_metrics_list.add_ap_metrics(ap_metrics=ap_metrics, legend=f"{epoch = }")
 
         batch_size = image_rgb.shape[0]
         training_metrics.update(
@@ -601,9 +604,24 @@ def simple_test():
         for key, value in loss_dict.items():
             training_metrics.update("Training", key, value.item(), count=batch_size, y_axis="Loss")
 
+        _, _, sorted_ap, conf_threshold = ap_metrics.get_best_sorted_ap()
+        training_metrics.update("Training", "Best sortedAP", sorted_ap, y_axis="sortedAP")
+        training_metrics.update(
+            "Training", "Conf thres of sortedAP", conf_threshold, y_axis="Conf threshold"
+        )
+
         training_metrics.end_loop(epoch)
+
+    ap_metrics_list.plot_ap_iou(
+        save_path="ap_iou.png",
+        title="Sorted AP curve",
+    )
+    ap_metrics_list.plot_sap_conf(
+        save_path="sap_conf.png",
+        title="Sorted AP w.r.t the confidence threshold",
+    )
 
 
 if __name__ == "__main__":
-    main()
-    # simple_test()
+    # main()
+    simple_test()